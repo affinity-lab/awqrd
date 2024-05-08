ff4ebdef7597b640c314eb0ef873839a9d8e62a9
Dav <divad.sari@gmail.com>
2024-05-08 12:32:18 +0200

    added jwt to util, and fixed preprocess middleware in comet.

--- packages/comet/src/middlewares/preprocess.ts
+++ packages/comet/src/middlewares/preprocess.ts
@@ -3,8 +3,10 @@ import type {CometState} from "../client/client";
 
 export class PreprocessMiddleware implements Middleware {
 	async handle(state: CometState, next: Function) {
-		if (typeof state.cmd.config.preprocess === "function") state.cmd.config.preprocess(state);
-		if (Array.isArray(state.cmd.config.preprocess)) state.cmd.config.preprocess.forEach(preprocess => preprocess(state));
+		if (typeof state.cmd.config.preprocess === "function") await state.cmd.config.preprocess(state);
+		if (Array.isArray(state.cmd.config.preprocess)) for (const preprocess of state.cmd.config.preprocess) {
+			await preprocess(state);
+		}
 		return await next()
 	}
 }
