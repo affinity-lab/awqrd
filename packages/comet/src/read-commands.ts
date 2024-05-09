999e23e553954834a8dda21ea50f8f9d49f1d8cf
Dav <divad.sari@gmail.com>
2024-05-09 13:53:45 +0200

    group thingy

--- packages/comet/src/read-commands.ts
+++ packages/comet/src/read-commands.ts
@@ -16,24 +16,25 @@ export function readCommands(commandsPath: string, clients: Record<string, Clien
 
 		if (config === undefined) throw Error("Config not found for " + store.target.name + ". Did you forget to add the @Comet decorator?");
 		let group = config["group"];
+		if(group) {
+			let cometInstance = new (store.target as new () => any)(); // TODO typehint
 
-		let cometInstance = new (store.target as new () => any)(); // TODO typehint
+			for (const key in config["command"]) {
+				let name = ((group["name"] || store.target.name) + "." + (config["command"][key]["name"] || key)).toLowerCase();
+				let clients = config["command"][key]["clients"] || group["clients"] || allClients;
 
-		for (const key in config["command"]) {
-			let name = ((group["name"] || store.target.name) + "." + (config["command"][key]["name"] || key)).toLowerCase();
-			let clients = config["command"][key]["clients"] || group["clients"] || allClients;
-
-			let params:string[] = [];
-			if(config["params"] !== undefined) {
-				for (const param in config["params"][key]) {
-					params[parseInt(param)] = config["params"][key][param];
+				let params:string[] = [];
+				if(config["params"] !== undefined) {
+					for (const param in config["params"][key]) {
+						params[parseInt(param)] = config["params"][key][param];
+					}
 				}
-			}
 
-			clients.forEach((client:Client) => {
-				config!["command"][key] = omitFields(config!["command"][key], "name", "clients")
-				client.add(name, cometInstance, key, config!["command"][key], params)
-			});
+				clients.forEach((client:Client) => {
+					config!["command"][key] = omitFields(config!["command"][key], "name", "clients")
+					client.add(name, cometInstance, key, config!["command"][key], params)
+				});
+			}
 		}
 	})
 }
