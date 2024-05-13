155450cc3b057ea4a3ac140b131488ad40a7b760
Dav <divad.sari@gmail.com>
2024-05-13 10:28:30 +0200

    sapphire error, and build

--- packages/sapphire/src/error.ts
+++ packages/sapphire/src/error.ts
@@ -3,6 +3,8 @@ import {createErrorData, preprocessErrorTree} from "@affinity-lab/util";
 
 export const sapphireError = {
 	notFound: (details: Record<string, any> = {}) => createErrorData("Not Found", details, 404),
+	unauthorized: () => createErrorData("Unauthorized", {}, 401),
+	forbidden: () => createErrorData("Forbidden", {}, 403),
 };
 
 preprocessErrorTree(sapphireError, "COMET");
