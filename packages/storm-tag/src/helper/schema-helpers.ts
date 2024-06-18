4dd4714b6950ec14506acc3450df4ac299303ad9
Dav <divad.sari@gmail.com>
2024-06-18 11:06:17 +0200

    (feature) group-tag-helper
    changed groupCol to reference

--- packages/storm-tag/src/helper/schema-helpers.ts
+++ packages/storm-tag/src/helper/schema-helpers.ts
@@ -1,19 +1,20 @@
-import {MySqlColumnBuilder, mysqlTable, varchar} from "drizzle-orm/mysql-core";
+import {mysqlTable, varchar} from "drizzle-orm/mysql-core";
 import {type MySqlIntBuilderInitial} from "drizzle-orm/mysql-core/columns/int";
+import {MySqlColumn, int} from "drizzle-orm/mysql-core";
 
 export let stormTagSchemaHelpers = {
 	tagTableFactory: function (name: string, id: () => MySqlIntBuilderInitial<any>) {
 		let columns = this.tagCols(id);
 		return mysqlTable(name, columns);
 	},
-	groupTagTableFactory: function (name: string, id: () => MySqlIntBuilderInitial<any>, groupCol: MySqlColumnBuilder) {
-		let columns = this.groupTagCols(id, groupCol);
+	groupTagTableFactory: function (name: string, id: () => MySqlIntBuilderInitial<any>, groupColReference: () => MySqlColumn) {
+		let columns = this.groupTagCols(id, groupColReference);
 		return mysqlTable(name, columns);
 	},
 	tagCols: function (id: () => MySqlIntBuilderInitial<any>) {
 		return {id: id(), name: varchar("name", {length: 255}).notNull().unique()};
 	},
-	groupTagCols: function (id: () => MySqlIntBuilderInitial<any>, groupCol: MySqlColumnBuilder) {
-		return {id: id(), name: varchar("name", {length: 255}).notNull().unique(), groupId: groupCol};
+	groupTagCols: function (id: () => MySqlIntBuilderInitial<any>, groupColReference: () => MySqlColumn) {
+		return {id: id(), name: varchar("name", {length: 255}).notNull().unique(), groupId: int("groupId").references(groupColReference)};
 	}
 }
