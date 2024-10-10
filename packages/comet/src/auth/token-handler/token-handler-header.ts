2e4362ea0e96673108d74b6dbbb8cf9536ed8751
Dav <divad.sari@gmail.com>
2024-10-10 12:36:14 +0200

    token auth added to comet

---
+++ packages/comet/src/auth/token-handler/token-handler-header.ts
@@ -0,0 +1,40 @@
+import type {AuthToken, RefreshToken, TokenEncoder, TokenHandler} from "../types";
+import type {CometResult} from "../../comet-result";
+import {CometState} from "../../client/comet-state";
+
+export class TokenHandlerHeader implements TokenHandler {
+	constructor(
+		protected tokenAdapter: TokenEncoder,
+		protected authTokenTimeout: number = 60 * 10, // 10 minutes
+		protected refreshTokenTimeout: number = 60 * 30, // 30 minutes
+		protected authTokenHeader: string = "auth-token",
+		protected refreshTokenHeader: string = "refresh-token",
+		protected staySignedInAuthTokenTimeout: number = 60 * 15, // 15 minutes
+		protected staySignedInRefreshTokenTimeout: number = 60 * 60 * 24 * 30, // 30 days
+	) {
+	}
+
+	setAuthToken(result: CometResult, value: AuthToken, staySignedIn: boolean = false) {
+		result.addHeader(this.authTokenHeader, this.tokenAdapter.createAuthToken(value, staySignedIn ? this.staySignedInAuthTokenTimeout : this.authTokenTimeout));
+	}
+
+	setRefreshToken(result: CometResult, value: RefreshToken, staySignedIn: boolean = false) {
+		result.addHeader(this.refreshTokenHeader, this.tokenAdapter.createRefreshToken(value, staySignedIn ? this.staySignedInRefreshTokenTimeout : this.refreshTokenTimeout));
+	}
+
+	deleteAuthToken(result: CometResult) {
+		result.addHeader(this.authTokenHeader, "");
+	}
+
+	deleteRefreshToken(result: CometResult) {
+		result.addHeader(this.refreshTokenHeader, "");
+	}
+
+	getAuthToken(state: CometState) {
+		return this.tokenAdapter.readAuthToken(state.headers[this.authTokenHeader]);
+	}
+
+	getRefreshToken(state: CometState) {
+		return this.tokenAdapter.readRefreshToken(state.headers[this.refreshTokenHeader]);
+	}
+}
--- packages/comet/src/middlewares/render.ts
+++ packages/comet/src/middlewares/render.ts
@@ -1,19 +1,20 @@
 import {CometState} from "../client/comet-state";
 import {CometResult} from "../comet-result";
 import {CometLogger} from "../types";
+import {Context} from "hono";
 
-export function renderMiddleware<REQ = any>(logger?: CometLogger<REQ>) {
+export function renderMiddleware<REQ extends Context = any>(logger?: CometLogger<REQ>) {
 	return async function (state: CometState<REQ>, next: () => Promise<CometResult>): Promise<Response> {
 		try {
 			if (logger?.requestLog) logger.requestLog(state);
 			let result = await next();
-			let response = Response.json(result.result, {status: result.status})
-			result.headers.forEach((h) => response.headers.append(h[0], h[1]));
+			let response = state.ctx.json(result.result, {status: result.status})
+			result.headers.forEach((h) => state.ctx.header(h[0], h[1]));
 			if (logger?.resultLog) logger.resultLog(state, result);
 			return response;
 		} catch (error) {
 			if (logger?.errorLog) logger.errorLog(state, error);
-			return Response.json("", {status: 500})
+			return state.ctx.json("", {status: 500})
 		}
 	}
 }
